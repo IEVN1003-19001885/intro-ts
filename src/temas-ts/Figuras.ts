82b78b1ab83d1854530f7486e000242a46f0dd39
Jessica Rocha <jessicarochamj@gmail.com>
2024-09-11 19:27:31 -0600

    commit uno practica 11 de septiembre 2024

---
+++ src/temas-ts/Figuras.ts
@@ -0,0 +1,13 @@
+//Área de Figuras: rectangulo, pentagono, circunferencia
+class Area{
+    protected pi:number;
+    protected radio:number;
+    constructor(pi:number, radio:number){
+        this.pi = pi;
+        this.radio = radio;
+
+    }
+    calculo(){
+        
+    }
+}
---
+++ src/temas-ts/Formula-general.ts
@@ -0,0 +1,39 @@
+//Formula General
+class FormulaGeneral {
+    a:number;
+    b:number;
+    c:number;
+    constructor(a:number, b:number, c:number) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+
+    calculo() {
+        return this.b ** 2 - 4 * this.a * this.c;
+    }
+
+
+    calcularRaiz() {
+        const d = this.calculo();
+        if (d < 0) {
+            return "Las raíces son complejas";
+        } else if (d === 0) {
+            const x = -this.b / (2 * this.a);
+            return `Raíz única: x1 = x2 = ${x}`;
+        } else {
+            const x1 = (-this.b + Math.sqrt(d)) / (2 * this.a);
+            const x2 = (-this.b - Math.sqrt(d)) / (2 * this.a);
+            return `Raíces reales: x1 = ${x1}, x2 = ${x2}`;
+        }
+    }
+}
+
+const a = 1;
+const b = -3;
+const c = 2;
+
+const ecuacion = new FormulaGeneral(a, b, c);
+
+console.log(ecuacion.calcularRaiz());
